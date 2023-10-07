9d0d3efa6abdc684c2a7787b2665784824dc4b33
junyi2022 <junyi2022@berkeley.edu>
2023-10-07 14:32:43 -0400

    finish week 4 recording

--- week04/demographics-dataviz-start/js/main.js
+++ week04/demographics-dataviz-start/js/main.js
@@ -1,58 +1,57 @@
+import * as csv from 'https://unpkg.com/csv-parse@5.5.1/dist/esm/sync.js'
+
 const map = L.map('census-map', {
-    zoomSnap: 0,
+    zoomSnap: 0, // if zoomSnap=1, zoom level have to be integer; if zoomSnap=0, can be anything
 });
 map.setView([39.96, -75.15], 11.5);
-//L is leaflet stuff, map start a map, latitude & longtitude
-//number is zoom in level, 11 is zoom-in level for entire world
+// L is leaflet stuff, map start a map, latitude & longtitude
+// number is zoom in level, 11 is zoom-in level for entire world
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer('https://api.mapbox.com/styles/v1/junyiy/clng7r3oq083901qx0eu9gaor/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoianVueWl5IiwiYSI6ImNsbXMza292bjAxcXoybG1meHhuZ3N1cjYifQ.EYo5VECxk9-NCAEgc3dm9w', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-//always put attrubution: cite sources
+// always put attrubution: cite sources
 
 async function downloadGeographicData() {
-    const resp = await fetch('../data/phl_blockgroups.geojson'); //making http request
-    const json = await resp.json(); //reading that file
-    return json;
+  const resp = await fetch('../data/phl_blockgroups.geojson'); // making http request from local
+  const json = await resp.json(); // reading that file
+  return json;
 }
 
-async function downloadDemographic() {
-    const resp = await fetch('../data/phl_blockgroup_dwg.csv'); //making http request
-    const test = await resp.text(); //reading that file
-    const rows = csv.parse(text, {
-        from_line: 2,
-        columns: false,
-    });
-    console.log(rows);
-
-    const dmgByGeoid = {};
-
-    for (const row of rows) {
-        const [state, county, tract, bgid] = row.slice(row.length - 4);
-        const geoid = state + county + tract + bgid;
-        row.push(geoid);
-        dmgByGeoid[geoid] = row;
-    }
-    return dmgByGeoid;
+async function downloadDemographicData() {
+  const resp = await fetch('../data/phl_blockgroup_dmg.csv'); // making http request from local server
+  const text = await resp.text(); // download content of that file, at this point it's just text
+  const rows = csv.parse(text, {
+    from_line: 2, // skip the first line
+    columns: false, // get array instead of objects
+  });
+  const dmgByGeoid = {};
+  for (const row of rows) { // current row of all the rows
+    const [state, county, tract, bgid] = row.slice(row.length - 4); // slice used to subset array, here take last four things from a row (shortcut)
+    const geoid = state + county + tract + bgid;
+    row.push(geoid);
+    dmgByGeoid[geoid] = row;
+  }
+  return dmgByGeoid;
 }
 
-const geoData = await downloadGeographicData();
-const dmgData = await downloadDemographic();
+const geoData = await downloadGeographicData(); // use function
+const dmgData = await downloadDemographicData();
 
 function calcFeatureStyle(blockgroup) {
-    const geoid = blockgroup.properties['GEOID'];
-    const row = dmgData[geoid];
-    const totalPop = row[0];
-    const maxPop = 4657;
+  const geoid = blockgroup.properties['GEOID'];
+  const row = dmgData[geoid];
+  const totalPop = row[0];
+  const maxPop = 4657;
 
-    return { 
-        fillOpacity: totalPop * 1.0 / maxPop,
-        weight: 0,
-    };
+  return { 
+    fillOpacity: totalPop * 1.0 / maxPop,
+    weight: 0,
+  };
 }
-const dataLayer = L.geoJSON(geoData, {
-    style: calcFeatureStyle,
+const dataLayer = L.geoJSON(geoData, { // create geojason layer using the data we read
+  style: calcFeatureStyle, // change the style of data, e.g. style:{color:'red'}
 });
 dataLayer.addTo(map);
 
@@ -60,4 +59,4 @@ window.map = map;
 window.geoData = geoData;
 window.dmgData = dmgData;
 window.dataLayer - dataLayer;
-//window command is globally avaliable
+// window command is globally avaliable, can use in WDT
